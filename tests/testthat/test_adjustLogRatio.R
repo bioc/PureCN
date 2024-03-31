daf175bf1f3df9f06274d98a9e8c9e7b627826be
Markus Riester <markus.riester@novartis.com>
2024-03-31 17:02:36 -0400

    Fix broken test.

--- tests/testthat/test_adjustLogRatio.R
+++ tests/testthat/test_adjustLogRatio.R
@@ -8,6 +8,7 @@ test_that("Function returns expected values for example coverage", {
      log.ratio.adjusted <- adjustLogRatio(log.ratio, purity, ploidy)
      total.ploidy <- 1.73
      p <- 1
+     log.ratio.offset <- 0
      opt.C <- (2^(log.ratio.adjusted + log.ratio.offset) *  total.ploidy)/p - ((2 * (1 - p))/p)
      expect_lt(abs(min(log.ratio.adjusted, na.rm=TRUE) - log2(0.004)), 0.001)
      expect_lt(median(abs(opt.C - purecn.example.output$results[[1]]$seg$C)), 0.1)  
